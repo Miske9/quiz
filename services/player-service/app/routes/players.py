a67f2c45c869a51f07c232eb46336f1b39f035e0
Matija Misan <matija.misan@outlook.com>
2026-08-31 21:24:48 +0200

    GET - players

--- services/player-service/app/routes/players.py
+++ services/player-service/app/routes/players.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from ..database import get_db
@@ -10,6 +10,24 @@ router = APIRouter(
     tags=["Players"]
 )
 
+@router.get("/", response_model=list[PlayerResponse])
+def get_players(db: Session = Depends(get_db)):
+    return db.query(Player).all()
+
+
+@router.get("/{player_id}", response_model=PlayerResponse)
+def get_player(player_id: int, db: Session = Depends(get_db)):
+    player = db.query(Player).filter(
+        Player.id == player_id
+    ).first()
+
+    if not player:
+        raise HTTPException(
+            status_code=404,
+            detail="Player not found"
+        )
+
+    return player
 
 @router.post("/", response_model=PlayerResponse)
 def create_player(
